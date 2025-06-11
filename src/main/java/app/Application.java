51b047899d0972bfbffb9f82ae5b7bf87296e524
Vlad Kim <raizel@MacBook-Air-Vlad.local>
2025-06-11 21:15:35 +0900

    Spring: autowire task with Dog, Qualifier, Timer

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,15 +6,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
-
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        AnnotationConfigApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         for (int i = 0; i < 5; i++) {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
+            System.out.println(bean.getTimer().getTime());
+            System.out.println(bean.getTimer().getTime());
         }
-    }
 
+
+        applicationContext.close();
+    }
 }
